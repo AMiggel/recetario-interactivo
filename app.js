8d712687f8bd00bf838939569bb956ea1734749d
AMiggel <antonnio762@gmail.com>
2017-10-18 15:51:24 -0500

    Arrgelos Frontend
    Commit en el trabajo, se crea la pagina de inicio y la pagina grafica

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+app.set("view engine","pug");
 
-app.set("view engine","pug")
 app.use(express.static("public"));
-app.get("/", function(req,res){
+
+app.get("/inicio", function(req,res){
 	res.render("index");
 
+});
+app.get("/graphics", function(req,res){
+	res.render("graphics")
 });
 
 app.listen(8080);
