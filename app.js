c77e33c3a4f8ce1f6f14c59eb7b57710acc149e1
AMiggel <antonnio762@gmail.com>
2017-11-01 21:27:40 -0500

    commit lds haciendo cosultas

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 var express = require('express');
 var mongoose = require ('mongoose');
+var recetas = require('./routes/recetas')
 var app = express();
 var bodyParser = require('body-parser');
 
 // connect data base  mongodb
- mongoose.connect("mongodb://localhost/recetario");
+mongoose.connect("mongodb://localhost/recetario");
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended: true }));  
 
 // entidades
-var recetaSchema={
-	name:String,
-	origen:String,
-	timeP:String,
-	instrucciones:String
-};
-var Receta = mongoose.model("receta",recetaSchema);
+var recetaSchema= mongoose.Schema({
+	name:{type:String,required: true},
+	origen:{type: String,required:true},
+	timeP:{type: String,required:true},
+	instrucciones:{type: String,required:true}
+});
+
+var RecetaModel = mongoose.model('receta',recetaSchema);
+recetas.setModel(RecetaModel);
+
+app.get('/recetas', recetas.index);
+app.get('/recetas/create', recetas.create);
+app.post('/recetas', recetas.store);
+app.get('/recetas/:id', recetas.show);
+app.get('/recetas/:id/edit', recetas.edit);
+
+
+
+
 
 var ingredienteSchema={
 	name:String,
@@ -43,15 +56,15 @@ app.get("/graphics", function(req,res){
 	res.render("graphics");
 });
 
-app.get("/recetas",function(req,res){
-	Receta.find(function(err,documento){
-	if(err){console.log(error)}
-	res.render("/inicio",{ recetas:documento})
+// app.get("/recetas",function(req,res){
+// 	Receta.find(function(err,documento){
+// 	if(err){console.log(error)}
+// 	res.render("/inicio",{ recetas:documento})
 
-	});
-	res.render("recetas");
+// 	});
+// 	res.render("recetas");
 
-});
+// });
 // method post
 app.post("/receta", function(req,res){
 
---
+++ routes/recetas.js
@@ -0,0 +1,36 @@
+var receta;
+exports.setModel = function(modelo){
+	receta = modelo;
+	};
+
+exports.index = function(req, res){
+   receta.find({},function(err,recetas){
+   	if(err){
+   		res.send('ha surgido un error')
+   	}else{
+   		res.render('recetas',{
+   			recetas: recetas
+   		});
+   	}
+   })
+};
+   //
+
+exports.create = function(req, res){
+   //
+};
+exports.store = function(req, res){
+   //
+};
+exports.show = function(req, res){
+   //
+};
+exports.edit = function(req, res){
+   //
+};
+exports.update = function(req, res){
+   //
+};
+exports.destroy = function(req, res){
+   //
+};
